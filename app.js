2eee1271893b222358c87535e88861e0237b3fc7
hmzugur <craxx67@gmail.com>
2021-04-10 01:10:51 +0300

    ejs template engine

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ const bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const userRoutes = require('./routes/user')
 
+app.set('view engine','ejs')
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(express.static('public'));
+
 
 //routes
 app.use('/admin',adminRoutes);
 app.use(userRoutes);
 
 
-
-
 app.use((req,res) => {
 
-      res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+      res.status(404).sendFile('404')
 
 });
-
 
 app.listen(3000,() => {
       console.log("3000 port listening")
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 router.get('/',(req,res,next) => {
 
-      res.sendFile(path.join(__dirname,'../','views','index.html'))
+      res.render('index')
 
 });
 
