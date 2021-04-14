d9866809de2ff2b1e6b27912110ed40fe0e331fa
hmzugur <craxx67@gmail.com>
2021-04-14 06:03:32 +0300

    ürünler list olarak alındı ve buna bağlı tablo olusturuldu

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,11 @@
 
-const products = []
+const products = [
+      {name:"Samsung S8",price:3500,imageUrl:'1-tel.jpg',description:'Güzel Tel'},
+      {name:"Samsung S9",price:4500,imageUrl:'2-tel.jpg',description:'iyi Tel'},
+      {name:"Samsung S10",price:5500,imageUrl:'1-tel.jpg',description:'İdare Tel'},
+      {name:"Samsung S11",price:6500,imageUrl:'2-tel.jpg',description:'Hoş Tel'},
+      {name:"Samsung S12",price:9500,imageUrl:'1-tel.jpg',description:'Harika Tel'},
+]
 
 
 module.exports = class Product{
