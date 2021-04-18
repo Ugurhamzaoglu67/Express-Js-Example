11ba5c996e21b6f6ef4067cfd8a9cadae6dbf74a
hmzugur <craxx67@gmail.com>
2021-04-18 21:18:23 +0300

    Product & Category Model oluşturuldu-> Sequelize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const adminRoutes= require('./routes/adminRoutes')
 const shopRoutes = require('./routes/shopRoutes')
 const errorsController = require('./controllers/errors')
 const bodyParser = require("body-parser");
-const sequelize = require('./utility/database')
+
 
 app.set('view engine','ejs')
 app.use(express.static('public'));
@@ -17,16 +17,6 @@ app.use(bodyParser.json())
 //______________________________ routes ___________________________________
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-
-
-sequelize
-    .authenticate()
-    .then(()=> {
-          console.log("Başarılı..")
-    })
-    .catch((err) => {
-          console.log(err)
-    })
 
 
 
--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,44 +1,19 @@
-const connection = require('../utility/database')
+const Sequelize = require('sequelize')
+const sequelize = require('../utility/database')
 
-module.exports = class Categories {
 
-    constructor(name,description) {
-        this.id = (categories.length + 1)
-        this.name = name
-        this.description = description
+const Category = sequelize.define('category', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement : true,
+        allowNull:false,
+        primaryKey:true
+    },
+    name:Sequelize.STRING,
+    description: {
+        type: Sequelize.STRING,
+        allowNull:true
     }
+})
 
-    //____________________________ (1) DB ye Kayıt YAPMA_________________
-    saveCategory() {
-      return  connection.execute('INSERT INTO  categories (name,description) VALUES (?,?)',
-            [this.name, this.description]
-            )
-    }
-
-    //____________________________ (2) Tüm categories Tablosunu Getirme _________________
-    static getAll() {
-        return connection.execute('SELECT * FROM categories')
-    }
-
-    //____________________________ (3) id 'ye Göre Getirme ________________________________
-    static getById(id) {
-        return connection.execute('SELECT * FROM categories WHERE id=?',[id])
-    }
-
-    //____________________________ (4) Ürün Güncelleme ________________________________
-    static update(category) {
-        return connection.execute('UPDATE categories SET categories.name=?, categories.description=?',
-            [category.name, category.description])
-    }
-
-
-    //____________________________ (5) id'ye Göre Ürün Silme________________________________
-    static deleteById(id) {
-       return connection.execute('DELETE FROM categories WHERE id=?',
-           [id])
-
-    }
-
-
-
-}
+module.exports = Category
